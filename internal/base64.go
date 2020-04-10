1fd06f6dca7fbebb9f06d7c609a3da2daf870d38
Sophearak Tha <t.sophearak@gmail.com>
2020-04-10 11:08:21 +0700

    Refactor var name

--- internal/base64.go
+++ internal/base64.go
@@ -10,8 +10,8 @@ import (
 
 // GetEncodedSvg func
 func GetEncodedSvg(pathToSvg string) string {
-	coverSqip, _ := os.Open(pathToSvg)
-	reader := bufio.NewReader(coverSqip)
+	sqip, _ := os.Open(pathToSvg)
+	reader := bufio.NewReader(sqip)
 	content, _ := ioutil.ReadAll(reader)
 	encoded := base64.StdEncoding.EncodeToString(content)
 	sqipTemp := "data:image/svg+xml;base64,%s"
