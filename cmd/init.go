b2e75d96735c6a9bdc74cc74844538cfd7fe9454
Sophearak Tha <t.sophearak@gmail.com>
2020-04-11 10:52:24 +0700

    Handle static assets

--- cmd/init.go
+++ cmd/init.go
@@ -1,17 +1,72 @@
 package cmd
 
 import (
-	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 
+	"github.com/fatih/color"
+	"github.com/gobuffalo/packr/v2"
 	"github.com/spf13/cobra"
 )
 
 // Init cmd
 var Init = &cobra.Command{
-	Use:   "init [collection name]",
+	Use:   "init [collection-name]",
 	Short: "Initialize photo collection",
 	Long:  `init is for initializing new photo collection.`,
+	Args:  cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("initializing...")
+		if _, err := os.Stat(args[0]); !os.IsNotExist(err) {
+			color.Yellow("`%s` already exists!", args[0])
+		}
+
+		err := os.MkdirAll(args[0], os.ModePerm)
+		if err != nil {
+			color.Yellow("%s", err)
+		}
+
+		folders := []string{
+			filepath.Join("photos", "cover"),
+			filepath.Join("photos", "avatar"),
+			filepath.Join("photos", "collection"),
+			filepath.Join(".moul", "assets"),
+			filepath.Join(".moul", "photos", "cover"),
+			filepath.Join(".moul", "photos", "avatar"),
+			filepath.Join(".moul", "photos", "collection"),
+		}
+		for _, folder := range folders {
+			os.MkdirAll(filepath.Join(".", args[0], folder), os.ModePerm)
+		}
+
+		box := packr.New("assets", "assets")
+
+		dpng, _ := box.FindString("default-skin.bc570585.png")
+		ioutil.WriteFile(
+			filepath.Join(args[0], ".moul", "assets", "default-skin.bc570585.png"),
+			[]byte(dpng), 0644)
+
+		dsvg, _ := box.FindString("default-skin.4565082c.svg")
+		ioutil.WriteFile(
+			filepath.Join(args[0], ".moul", "assets", "default-skin.4565082c.svg"),
+			[]byte(dsvg), 0644)
+
+		pgif, _ := box.FindString("preloader.cd849b38.gif")
+		ioutil.WriteFile(
+			filepath.Join(args[0], ".moul", "assets", "preloader.cd849b38.gif"),
+			[]byte(pgif), 0644)
+
+		mcss, _ := box.FindString("moul.css")
+		ioutil.WriteFile(
+			filepath.Join(args[0], ".moul", "assets", "moul.css"), []byte(mcss), 0644,
+		)
+
+		mjs, _ := box.FindString("moul.js")
+		ioutil.WriteFile(
+			filepath.Join(args[0], ".moul", "assets", "moul.js"), []byte(mjs), 0644,
+		)
+
+		mtoml, _ := box.FindString("moul.toml")
+		ioutil.WriteFile(filepath.Join(args[0], "moul.toml"), []byte(mtoml), 0644)
 	},
 }
--- go.mod
+++ go.mod
@@ -10,15 +10,22 @@ require (
 	github.com/fogleman/gg v1.3.0 // indirect
 	github.com/fogleman/primitive v0.0.0-20190214200932-673f57e7b1b5 // indirect
 	github.com/gobuffalo/helpers v0.6.1 // indirect
+	github.com/gobuffalo/packr/v2 v2.8.0
 	github.com/gobuffalo/plush v3.8.3+incompatible
 	github.com/gobuffalo/tags v2.1.7+incompatible // indirect
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/gosimple/slug v1.9.0
+	github.com/karrick/godirwalk v1.15.6 // indirect
 	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
-	github.com/spf13/cobra v0.0.6
+	github.com/sirupsen/logrus v1.5.0 // indirect
+	github.com/spf13/cobra v1.0.0
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/spf13/viper v1.6.2
 	github.com/tdewolff/minify v2.3.6+incompatible // indirect
 	github.com/tdewolff/minify/v2 v2.7.3
 	github.com/tdewolff/parse v2.3.4+incompatible // indirect
+	golang.org/x/crypto v0.0.0-20200406173513-056763e48d71 // indirect
+	golang.org/x/sync v0.0.0-20200317015054-43a5402ce75a // indirect
+	golang.org/x/sys v0.0.0-20200409092240-59c9f1ba88fa // indirect
+	golang.org/x/tools v0.0.0-20200410194907-79a7a3126eef // indirect
 )
