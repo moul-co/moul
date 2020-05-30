c883229a174a01bd4368225447aab80d5e39ec3c
Sophearak Tha <t.sophearak@gmail.com>
2020-05-30 11:50:44 +0700

    Revert "Add `@angular/elements` and remove `@angular/forms`"
    This reverts commit 8658bf59e87daa66fcab552762a3000def27c67a.

--- moul-element/src/polyfills.ts
+++ moul-element/src/polyfills.ts
@@ -60,4 +60,3 @@ import 'zone.js/dist/zone' // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
-import 'document-register-element'
