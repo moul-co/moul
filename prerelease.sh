3d71bbf6a1444d97c53c429e57350ec90eabfd7b
Phearak S. Tha <t.sophearak@gmail.com>
2022-04-24 14:12:13 +0700

    Update `prerelease.sh`

--- prerelease.sh
+++ prerelease.sh
@@ -6,4 +6,4 @@ cd build && ncc build index.js -o dist && mv dist/index.js ../cmd/boilerplate/de
 cd .. && rm -rf cmd/boilerplate/dev/build && mv .moul/public/build cmd/boilerplate/dev/
 mv cmd/boilerplate/dev/build/_assets cmd/boilerplate/dev/build/assets
 mv cmd/boilerplate/dev/build/_shared cmd/boilerplate/dev/build/shared
-rm -rf cmd/boilerplate/app && cp -R app cmd/boilerplate/
+rm -rf cmd/boilerplate/app && cp -R app cmd/boilerplate/ && rm -rf cmd/boilerplate/app/data
