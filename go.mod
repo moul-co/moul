d813f65918ebcd79ccf2d5e479597a7ab24376cd
Phearak S. Tha <t.sophearak@gmail.com>
2022-12-27 22:24:55 +0700

    Initial support for resize

--- go.mod
+++ go.mod
@@ -3,26 +3,29 @@ module github.com/moul-co/moul
 go 1.19
 
 require (
+	github.com/bbrks/go-blurhash v1.1.1
+	github.com/catcombo/go-staticfiles v0.4.0
+	github.com/disintegration/imaging v1.6.2
+	github.com/evanw/esbuild v0.16.8
 	github.com/gofiber/fiber/v2 v2.40.1
+	github.com/gofiber/template v1.7.3
+	github.com/otiai10/copy v1.9.0
 	github.com/urfave/cli/v2 v2.23.7
 )
 
 require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
-	github.com/catcombo/go-staticfiles v0.4.0 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
-	github.com/evanw/esbuild v0.16.8 // indirect
-	github.com/gofiber/template v1.7.3 // indirect
 	github.com/klauspost/compress v1.15.13 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/mattn/go-runewidth v0.0.14 // indirect
-	github.com/otiai10/copy v1.9.0 // indirect
 	github.com/rivo/uniseg v0.4.3 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.43.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8 // indirect
 	golang.org/x/sys v0.3.0 // indirect
 )
--- wasm/main.go
+++ wasm/main.go
@@ -4,8 +4,16 @@
 package main
 
 import (
+	"bytes"
 	"embed"
+	"encoding/base64"
+	"errors"
+	"image/jpeg"
+	"math"
 	"syscall/js"
+
+	"github.com/bbrks/go-blurhash"
+	"github.com/disintegration/imaging"
 )
 
 //go:embed templates
@@ -13,37 +21,90 @@ var templates embed.FS
 
 func main() {
 	// renderer for markdoc renderable tree
-	js.Global().Set("moulRenderer", moulRenderer())
+	js.Global().Set("moulifyContent", moulifyContent())
 	// resize photo to maximum 4096
-	js.Global().Set("moulResize", moulResize())
+	js.Global().Set("moulifyPhoto", moulifyPhoto())
 
 	<-make(chan bool)
 }
 
-func moulRenderer() js.Func {
+func moulifyContent() js.Func {
 	return js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 		handler := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 			resolve := args[0]
+			reject := args[1]
 
 			go func() {
 				layout, _ := templates.ReadFile("templates/layout.gohtml")
+
+				errr := js.Global().Get("Error").New(errors.New("something went wrong").Error())
+
 				resolve.Invoke(string(layout))
+				reject.Invoke(errr)
 			}()
 
 			return nil
 		})
 
-		promiseConstructor := js.Global().Get("Promise")
-		return promiseConstructor.New(handler)
+		// https://withblue.ink/2020/10/03/go-webassembly-http-requests-and-promises.html
+		promise := js.Global().Get("Promise")
+		return promise.New(handler)
 	})
 }
 
-func moulResize() js.Func {
+func moulifyPhoto() js.Func {
 	return js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-		if len(args) != 2 {
-			return "Need 2 args"
-		}
+		photo := make([]uint8, args[0].Get("byteLength").Int())
+		js.CopyBytesToGo(photo, args[0])
+		pr := bytes.NewReader(photo)
 
-		return ""
+		handler := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+			resolve := args[0]
+			reject := args[1]
+			go func() {
+				imgDecode, err := imaging.Decode(pr, imaging.AutoOrientation(true))
+				if err != nil {
+					reject.Invoke(err.Error())
+				}
+
+				ratio := math.Min(32/float64(imgDecode.Bounds().Dx()), 32/float64(imgDecode.Bounds().Dy()))
+				thumbnail := imaging.Resize(imgDecode, int(float64(imgDecode.Bounds().Dx())*ratio), 0, imaging.Lanczos)
+
+				ratioComponent := math.Min(5/float64(thumbnail.Bounds().Dx()), 5/float64(thumbnail.Bounds().Dy()))
+				xCom := float64(thumbnail.Bounds().Dx()) * ratioComponent
+				yCom := float64(thumbnail.Bounds().Dy()) * ratioComponent
+				hash, err := blurhash.Encode(int(xCom), int(yCom), thumbnail)
+				if err != nil {
+					reject.Invoke(err.Error())
+				}
+				w := float64(thumbnail.Bounds().Dx()) * ratio
+				h := float64(thumbnail.Bounds().Dy()) * ratio
+				decodedB64, err := blurhash.Decode(hash, int(w), int(h), 1)
+				if err != nil {
+					reject.Invoke(err.Error())
+				}
+
+				b64Buf := new(bytes.Buffer)
+				err = jpeg.Encode(b64Buf, decodedB64, &jpeg.Options{Quality: 95})
+				if err != nil {
+					reject.Invoke(err.Error())
+				}
+
+				resolve.Invoke(
+					map[string]interface{}{
+						"width":    imgDecode.Bounds().Dx(),
+						"height":   imgDecode.Bounds().Dy(),
+						"blurhash": base64.StdEncoding.EncodeToString(b64Buf.Bytes()),
+						"xl":       nil,
+						"lg":       nil,
+						"md":       nil,
+						"sm":       nil,
+					},
+				)
+			}()
+			return nil
+		})
+		promise := js.Global().Get("Promise")
+		return promise.New(handler)
 	})
 }
