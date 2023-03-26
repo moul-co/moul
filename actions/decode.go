b3500ae1bdeaf168e43554c1aa6295f25eb72b6b
Phearak S. Tha <t.sophearak@gmail.com>
2023-03-26 16:23:53 +0700

    Support decode thumbhash

--- actions/decode.go
+++ actions/decode.go
@@ -3,17 +3,21 @@ package actions
 import (
 	"bytes"
 	"encoding/base64"
-	"errors"
 	"fmt"
+	"image"
 	"image/jpeg"
 	"math"
 
 	"github.com/bbrks/go-blurhash"
+	"github.com/galdor/go-thumbhash"
 	"github.com/urfave/cli/v2"
 )
 
 func Decode(ctx *cli.Context) error {
 	bh := ctx.String("blurhash")
+	var err error
+	var img image.Image
+	bf := new(bytes.Buffer)
 
 	if bh != "" {
 		width := ctx.Float64("width")
@@ -21,21 +25,29 @@ func Decode(ctx *cli.Context) error {
 
 		ratio := math.Min(32/float64(width), 32/float64(height))
 
-		img, err := blurhash.Decode(bh, int(width*ratio), int(height*ratio), 1)
+		img, err = blurhash.Decode(bh, int(width*ratio), int(height*ratio), 1)
 		if err != nil {
 			return err
 		}
-
-		xsb := new(bytes.Buffer)
-		jpeg.Encode(xsb, img, &jpeg.Options{Quality: 95})
-		fmt.Printf("data:image/jpeg;charset=utf-8;base64,%v", base64.StdEncoding.EncodeToString(xsb.Bytes()))
-
 	}
 
 	th := ctx.String("thumbhash")
 	if th != "" {
-		return errors.New("not support yet")
+		hash, err := base64.StdEncoding.DecodeString(th)
+		if err != nil {
+			return err
+		}
+
+		var cfg thumbhash.DecodingCfg
+		cfg.SaturationBoost = 1.5
+		img, err = thumbhash.DecodeImageWithCfg(hash, cfg)
+		if err != nil {
+			return err
+		}
 	}
+
+	jpeg.Encode(bf, img, &jpeg.Options{Quality: 95})
+	fmt.Printf("data:image/jpeg;charset=utf-8;base64,%v", base64.StdEncoding.EncodeToString(bf.Bytes()))
 
 	return nil
 }
